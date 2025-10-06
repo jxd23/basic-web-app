88c3213db26896033818ddd2abd6e7e96c36a7c9
Josh Dong <joshuado@andrew.cmu.edu>
2025-10-06 13:31:48 -0400

    added andrewid query function and tests

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -18,6 +18,14 @@ describe("QueryProcessor", () => {
           ));
     });
 
+    test('should return andrewid', () => {
+        const query = "andrew id";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "joshuado"
+          ));
+    });
+
     test('should return name', () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
